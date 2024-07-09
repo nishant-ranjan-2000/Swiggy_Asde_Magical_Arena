09789f41b9a108f53cbfaee34a4a62613d17f4ae
nishant-ranjan-2000 <ranjannishant98@gmail.com>
2024-07-09 23:59:10 +0530

    Added Player class in Entity package

---
+++ _MagicalArena/MagicalArena/src/main/java/com/SwiggyRound1/MagicalArena/entity/Player.java
@@ -0,0 +1,31 @@
+package com.SwiggyRound1.MagicalArena.entity;
+
+public class Player {
+	private int health;
+	private int strength;
+	private int attack;
+	public Player(int health, int strength, int attack) {
+		super();
+		this.health = health;
+		this.strength = strength;
+		this.attack = attack;
+	}
+	public int getHealth() {
+		return health;
+	}
+	public void setHealth(int health) {
+		this.health = health;
+	}
+	public int getStrength() {
+		return strength;
+	}
+	public void setStrength(int strength) {
+		this.strength = strength;
+	}
+	public int getAttack() {
+		return attack;
+	}
+	public void setAttack(int attack) {
+		this.attack = attack;
+	}
+}
